09ede2d68b6df0ca4a28e6313a9c31839416b370
Sonar <80487755+RealSonarX@users.noreply.github.com>
2025-01-20 21:19:48 +0000

    Initial commit

---
+++ src/meteor_bot/lists.py
@@ -0,0 +1,17 @@
+meta_knight = f" Meta Knight is everything, he's all you ever wanted \n You could win a set now, even if you've never " \
+              f"won it \n Clashes he always wins, trade they ain't never gonna \n You want him forever any counter " \
+              f"pick he's better on it \n Aerial is down, press it while I'm jumpin' up \n Then hit the B button like " \
+              f"i don't give a fuck \n I know you wait for it, but you're out of luck \n I can do the same thing " \
+              f"every single time \n I say Meta Knight da bess, Meta Knight da bess \n Meta Knight da bess, " \
+              f"Meta Knight da bess \n Now Donkey Kong is mad, Dedede is mad \n Mario is mad, Ice Climbers are mad "
+
+roy = (
+                    "Why the FUCK does Roy’s jab come out faster than Brawl Meta Knight up air like holy SHIT I wanna "
+                    "kms every time I play against this foreskin-munching BITCH ASS CHARACTER. If I ever have to play "
+                    "against this skilless, dickless, bitchless, end lag-less character IN MY LIFE I will end my life "
+                    "fr. To all Roy players out there, die. Literally just die. I hope your dick fucking explodes in "
+                    "a horrible accident and your house burns down and your switch and your router fucking explode "
+                    "with your dick so you can stop plaguing this game with your bitch ass gameplay. I fucking hate "
+                    "your bitch ass long ass sword which has more range than my fucking boomerang like what the fuck "
+                    "there’s no way Roy is actually a character in this fucking game. Fuck you and I hope your family "
+                    "is brutally molested by 7 BBCs in an alleyway in Chicago.")
---
+++ src/meteor_bot/main.py
@@ -0,0 +1,129 @@
+from discord import *
+from datetime import *
+from .lists import *
+from os import getenv
+from random import *
+from discord.ext import commands
+from dotenv import load_dotenv
+
+def main():
+
+    intents = Intents.all()
+
+    bot = commands.Bot(command_prefix='.', intents=intents)
+
+    load_dotenv("token.env")
+    TOKEN = getenv('TOKEN')
+
+
+    # tree = app_commands.CommandTree(bot)
+
+    @bot.tree.command(name='vengeance', description='MY REVENGGGGEEEE')
+    async def vengeance(ctx):
+        channel = ctx.channel
+        print(channel)
+        for i in range(0, randint(5, 7)):
+            await channel.send("<@1095350739301310674>", delete_after=randint(0, 5))
+
+
+    @bot.tree.command(name='newjoins', description='Find new joins')
+    async def newjoins(interaction, elim_date: str):
+        guild = bot.get_guild(906804682452779058)
+        channel = interaction.channel.name
+        for e in guild.channels:
+            if str(e.name) == str(channel):
+                print("Channel")
+                channel = e
+        new_joins = []
+        i = (datetime.strptime(elim_date, "%d/%m/%Y").date())
+        print(i)
+        for member in guild.members:
+            e = (member.joined_at.date())
+            # print(e)
+            if e >= i:
+                print(member.name)
+                new_joins.append(f"{member.name}")
+        await interaction.response.send_message(f"Getting list of new users after {elim_date}! Check Python Terminal")
+        complete_message = '\n'.join(new_joins)
+        await channel.send(f"```{complete_message}```")
+
+
+    async def timeout_user(member: Member):
+        await member.timeout(timedelta(seconds=5), reason=f"Joe")
+
+
+    @bot.event
+    async def on_message(message):
+        # print(message.author)
+        print(f"New Message : {message.content}")
+        if str(message.author) != 'Meteor#1277':
+
+            if 'meta knight' in message.content.lower():
+                await message.channel.send(meta_knight)
+            elif 'roy' in message.content.lower():
+                if randint(0, 10) == 1:
+                    await message.channel.send(roy)
+            elif ':jojer:' in message.content:
+                if randint(0, 5) == 1:
+                    await message.channel.send(
+                        "Whal's literally built like the guy who gets his ass kicked by the mc at the beginning of a "
+                        "karate movie")
+            elif '<@&1328843759764639895>' in message.content:
+                await message.channel.send('https://tenor.com/view/cat-but-heres-the-yapping-gif-5342913541658644726')
+            elif 'aegis' in message.content.lower():
+                await message.channel.send("")
+            # elif str(message.author) == 'randomness8736':
+            #    await message.channel.send("I didn't ask", delete_after=5)
+            #    await timeout_user(message.author)
+
+
+    async def reset_username(member: Member):
+        if str(member) == '1mpy':
+            chosen_username = str(illu_names[randint(0, (len(illu_names)) - 1)])
+            print(chosen_username)
+            await member.edit(nick=chosen_username)
+
+
+    @bot.event
+    async def on_member_update(before, after):
+        channel = bot.get_channel(956606255974199327)
+        print(channel)
+        if str(after) == '':
+            pass
+            # if str(after.nick) not in :
+            #    await channel.send(f'Nice try {after.mention}', delete_after=1)
+            #    await reset_username(after)
+
+
+    #    if str(after) == '_the_aegis_':
+    #        if str(after.nick) not in illu_names and before.nick != after.nick:
+    #            await channel.send(f'Nice try {after.mention}', delete_after=1)
+    #            await reset_username(after)
+
+
+    @bot.event
+    async def on_message_edit(before, after):
+        author = before.author
+        channel = before.channel
+        if before.content != after.content and randint(0, 15) == 3:
+            await channel.send(f"I saw that {author.mention}", delete_after=0.1)
+
+
+    @bot.event
+    async def on_member_remove(member):
+        channel = bot.get_channel(956606255974199327)
+        await channel.send(f"{member} finally buggered off")
+        # print(channel)
+
+
+    @bot.event
+    async def on_ready():
+        print(f'We have logged in as {bot.user}')
+        await bot.tree.sync()
+
+
+    bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
