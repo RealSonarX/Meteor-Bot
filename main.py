82e63f6572f54b3c894edb437d3c913e03d047a0
Sonar <80487755+RealSonarX@users.noreply.github.com>
2025-01-24 18:31:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+from discord import *
 from datetime import *
-from random import randint
-
-from discord import Intents
-from discord.ext import commands
-
 from lists import *
+from os import getenv
+from random import *
+from discord.ext import commands
+from dotenv import load_dotenv
 
 intents = Intents.all()
 
 bot = commands.Bot(command_prefix='.', intents=intents)
 
+load_dotenv("token.env")
+TOKEN = getenv('TOKEN')
+# tree = app_commands.CommandTree(bot)
 
 @bot.tree.command(name='vengeance', description='MY REVENGGGGEEEE')
 async def vengeance(ctx):
@@ -41,11 +44,16 @@ async def newjoins(interaction, elim_date : str):
     complete_message = '\n'.join(new_joins)
     await channel.send(f"```{complete_message}```")
 
+async def timeout_user(member: Member):
+    await member.timeout(timedelta(seconds=5), reason=f"Joe")
+
+
 
 
 @bot.event
 async def on_message(message):
     # print(message.author)
+    print(f"New Message : {message.content}")
     if str(message.author) != 'Meteor#1277':
 
         if 'meta knight' in message.content.lower():
@@ -58,11 +66,36 @@ async def on_message(message):
                 await message.channel.send(
                     "Whal's literally built like the guy who gets his ass kicked by the mc at the beginning of a "
                     "karate movie")
+        elif '<@&1328843759764639895>' in message.content:
+            await message.channel.send('https://tenor.com/view/cat-but-heres-the-yapping-gif-5342913541658644726')
+        elif 'aegis' in message.content.lower():
+            await message.channel.send("")
+        #elif str(message.author) == 'randomness8736':
+        #    await message.channel.send("I didn't ask", delete_after=5)
+        #    await timeout_user(message.author)
+
+async def reset_username(member: Member):
+    if str(member) == '1mpy':
+        chosen_username = str(illu_names[randint(0, (len(illu_names)) - 1)])
+        print(chosen_username)
+        await member.edit(nick=chosen_username)
+
+@bot.event
+async def on_member_update(before, after):
+    channel = bot.get_channel(956606255974199327)
+    print(channel)
+    if str(after) == '':
+       if str(after.nick) not in illu_names:
+           await channel.send(f'Nice try {after.mention}', delete_after=1)
+           await reset_username(after)
 
 
-#@bot.event
-#async def on_member_update(before, after):
-#   channel = bot.get_channel(956606255974199327)
+#    if str(after) == '_the_aegis_':
+#        if str(after.nick) not in illu_names and before.nick != after.nick:
+#            await channel.send(f'Nice try {after.mention}', delete_after=1)
+#            await reset_username(after)
+
+# await channel.send(before)
 
 @bot.event
 async def on_message_edit(before, after):
@@ -85,9 +118,16 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
     await bot.tree.sync()
 
+    # await tree.sync(guild=(Object(id=906804682452779058)))
+
+
+# @tree.command(name = "rq", description = "a")
+# async def time(interaction):
+#    await interaction.response.send_message(f"Test")
+
+
+# await interaction.response.send_message("command")
 
 
 
-token = ''
-
-bot.run(token)
+bot.run(TOKEN)
